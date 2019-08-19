cf6b839aacdc5f213b480e6d377ae67a5eca4169
Jake Lee <kc80.lee@samsung.com>
2019-08-19 10:39:12 +0900

    1411

---
+++ problem-solving/jungol/1411-dp.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAX_N 100000
+#define NUM 20100529
+int N;
+int d[MAX_N] = {0, 1, 3};
+
+int dp(int n) {
+  if (n <= 0)
+    return 0;
+  if (d[n] > 0)
+    return d[n];
+
+  int d1 = dp(n - 1);
+  int d2 = dp(n - 2);
+
+  return d[n] = d1 + 2 * d2;
+}
+
+int main() {
+  // freopen("input.txt", "r", stdin);
+
+  scanf("%d", &N);
+  printf("%d\n", N);
+  printf("%d", dp(N));
+  
+  return 0;
+}
