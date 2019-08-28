d0a4e8b2afac7f3e9a69d2e149b36407f86fd732
Jake Lee <kc80.lee@samsung.com>
2019-08-28 14:33:45 +0900

    union-find

---
+++ problem-solving/acmicpc/1976-union-find.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define MAX_N 200
+#define MAX_M 1000
+
+int input[MAX_N + 1][MAX_N + 1];
+int city[MAX_N + 1];
+int id[MAX_N + 1];
+int lv[MAX_N + 1];
+int query[MAX_M];
+
+int N, M;
+
+void initArr(int size, int querySize) {
+  for (int i = 1; i <= size; i++) {
+    for (int j = 1; j <= size; j++) {
+      scanf("%d", &input[i][j]);
+    }
+
+    city[i] = i;
+    id[i] = i;
+    lv[i] = 1;
+  }
+
+  for (int i = 0; i < querySize; i++) {
+    scanf("%d", &query[i]);
+  }
+}
+
+int root(int p) {
+  while (p != id[p])
+    p = id[p];
+
+  return p;
+}
+
+bool isConnected(int p, int q) { return root(p) == root(q); }
+
+void unionFunc(int p, int q) {
+  if (isConnected(p, q))
+    return;
+
+  int pRoot = root(p);
+  int qRoot = root(q);
+
+  if (lv[pRoot] >= lv[qRoot]) { // p is parent
+    id[qRoot] = pRoot;
+    if (lv[pRoot] == lv[qRoot])
+      lv[pRoot]++;
+  } else { // q is parent
+    id[pRoot] = qRoot;
+  }
+}
+
+int main() {
+  // freopen("acmicpc/1976-union-find.txt", "r", stdin);
+  setbuf(stdout, NULL);
+
+  scanf("%d", &N);
+  scanf("%d", &M);
+  initArr(N, M);
+
+  for (int i = 1; i <= N; i++) {
+    for (int j = i + 1; j <= N; j++)
+      if (input[i][j])
+        unionFunc(i, j);
+  }
+
+  bool result = true;
+  for (int i = 0; i < M - 1; i++) {
+    if (!isConnected(query[i], query[i + 1])) {
+      result = false;
+      break;
+    }
+  }
+
+  if (result)
+    printf("YES");
+  else
+    printf("NO");
+}
---
+++ problem-solving/union-find.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_N 10000000
+
+int N = 10;
+int QUERY = 12;
+int input[MAX_N];
+int id[MAX_N];
+int lv[MAX_N];
+
+int root(int p) {
+  while (p != id[p])
+    p = id[p];
+
+  return p;
+}
+
+bool isConnected(int p, int q) { return root(p) == root(q); }
+
+void unionFunc(int p, int q) {
+  if (isConnected(p, q))
+    return;
+
+  int pRoot = root(p);
+  int qRoot = root(q);
+
+  if (lv[pRoot] >= lv[qRoot]) { // p is parent
+    id[qRoot] = pRoot;
+    if (lv[pRoot] == lv[qRoot])
+      lv[pRoot]++;
+  } else { // q is parent
+    id[pRoot] = qRoot;
+  }
+}
+
+void initArr(int size) {
+  srand(time(NULL));
+  for (int i = 0; i < size; i++) {
+    input[i] = i;
+    id[i] = i;
+    lv[i] = 1;
+  }
+}
+
+void printArr(int size) {
+  for (int i = 0; i < size; i++) {
+    printf("[%3d]%3d, id:%3d, root:%d\n", i, input[i], id[i], root(i));
+  }
+  printf("\n");
+}
+
+int main() {
+  freopen("union-find.txt", "r", stdin);
+  scanf("%d %d\n", &N, &QUERY);
+
+  initArr(N);
+  printArr(N);
+
+  char command;
+  int a, b;
+
+  time_t t = clock();
+  for (int i = 0; i < QUERY; i++) {
+    scanf("%c %d %d\n", &command, &a, &b);
+    if (command == 'u')
+      unionFunc(a, b);
+    else if (command == 'c')
+      printf("%d\n", isConnected(a, b));
+    printArr(N);
+  }
+  printf("elapsed time: %.3f\n", (clock() - t) / 1000.);
+
+  return 0;
+}
