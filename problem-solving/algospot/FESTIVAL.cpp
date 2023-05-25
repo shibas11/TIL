67f165f0b5e4c9795787f7a78955b840ed2514b0
Jake Lee <shibas11@gmail.com>
2023-05-26 03:45:45 +0900

    try again

--- problem-solving/algospot/FESTIVAL.cpp
+++ problem-solving/algospot/FESTIVAL.cpp
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
-#define MAX_N 1000
 #define MAX_C 100
+#define MAX_N 1000
+#define MAX_L 1000
 
 int C, N, L;
-int input[MAX_N + 1];
-int d[MAX_N + 1] = {0};
-float tmp;
+int input_sum[MAX_N + 1] = { 0, };
+double sum, answer, temp;
 
 int main() {
-  freopen("algospot/FESTIVAL.txt", "r", stdin);
-  double answer, tmp;
+    freopen("algospot/FESTIVAL.txt", "r", stdin);
 
-  scanf("%d", &C);
-  while (C--) {
-    answer = 100000;
+    scanf("%d", &C);
+    for (int c = 0; c < C; c++) {
+        scanf("%d %d", &N, &L);
+        for (int i = 1; i <= N; i++) {
+            scanf("%d", input_sum + i);
+            input_sum[i] += input_sum[i - 1]; // 1���� �����̰�, [0]���� 0�� ����־ ��������
+        }
 
-    scanf("%d %d", &N, &L);
-    for (int i = 1; i <= N; i++) {
-      scanf("%d", &input[i]);
-      d[i] = d[i - 1] + input[i];
+        answer = 1000000;
+        for (int n = 1; n <= N - L + 1; n++) {
+            for (int i = n; i <= N; i++) {
+                if ((i - n + 1) < L)
+                    continue;
+                sum = input_sum[i] - input_sum[n - 1];
+                temp = sum / (i - n + 1);
+                if (temp < answer) answer = temp;
+                //printf("%d %d %.11f\n", n, i, answer); // range�� ���� üũ
+            }
+        }
+        printf("%.11f\n", answer);
     }
 
-    for (int i = 1; i <= N - L + 1; i++) {
-      for (int j = i + L - 1; j <= N; j++) {
-        tmp = (double)(d[j] - d[i] + input[i]) / (j - i + 1);
-        answer = tmp < answer ? tmp : answer;
-      }
-    }
-
-    printf("%.12f\n", answer);
-  }
-
-  return 0;
+    return 0;
 }
